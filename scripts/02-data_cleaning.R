0f02820b6832ce9d4d0ebbac7fe01a685f982c8a
ruibosun <97702222+ruibosun@users.noreply.github.com>
2023-03-17 16:44:17 -0400

    select more variables change the plots and tables

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -21,7 +21,7 @@ raw_data <-
 
 raw_data <- 
   raw_data %>% 
-  select(mapres10,papres10,paeduc,maeduc,educ)
+  dplyr::select(mapres10,papres10,paeduc,maeduc,educ,mawrkslf,pawrkslf,degree,born,sex,reg16,incom16)
 
 #change class
 raw_data$mapres10 <- as.character(raw_data$mapres10)
@@ -30,30 +30,39 @@ raw_data$maeduc <- as.character(raw_data$maeduc)
 raw_data$paeduc <- as.character(raw_data$paeduc)
 raw_data$educ <- as.character(raw_data$educ)
 
-#change no formal to 0
-cleaned_data<-raw_data%>%mutate_all(~ifelse(. == "no formal schooling", 0, .))%>%
-  mutate_all(as.numeric)
+# Replace "no formal schooling" with 0 in the specified column
+raw_data$educ[raw_data$educ== "no formal schooling"] <- 0
+raw_data$maeduc[raw_data$maeduc== "no formal schooling"] <- 0
+raw_data$paeduc[raw_data$paeduc== "no formal schooling"] <- 0
+
+# Change to numeric
+raw_data$mapres10 <- as.numeric(raw_data$mapres10)
+raw_data$papres10 <- as.numeric(raw_data$papres10)
+raw_data$maeduc <- as.numeric(raw_data$maeduc)
+raw_data$paeduc <- as.numeric(raw_data$paeduc)
+raw_data$educ <- as.numeric(raw_data$educ)
 
 #group variables into levels
 
-mepa<-median(cleaned_data$papres10,na.rm=TRUE)
-mema<-median(cleaned_data$mapres10,na.rm=TRUE)
+mepa<-median(raw_data$papres10,na.rm=TRUE)
+mema<-median(raw_data$mapres10,na.rm=TRUE)
 
-cleaned_data <- cleaned_data %>%
+raw_data <- raw_data %>%
   mutate(father_factor = case_when(
     papres10 < mepa ~ "Lower than Median",
     papres10 >= mepa ~ "Above and Equal to Median"
   ))
 
-cleaned_data <- cleaned_data %>%
+raw_data <- raw_data %>%
   mutate(mother_factor = case_when(
     mapres10 < mema ~ "Lower than Median",
     mapres10 >= mema ~ "Above and Equal to Median"
   ))
 
+cleaned_data<-raw_data
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change cleaned_data to whatever name you end up with at the end of cleaning
-write_csv(cleaned_data, "inputs/data/GSS2021.dta")
+write_csv(cleaned_data, "inputs/data/GSS2021 2.csv")
 
