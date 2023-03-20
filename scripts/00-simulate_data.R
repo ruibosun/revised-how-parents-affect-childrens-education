de195f19a3cd499a2e2521ee3ed2f2275a7091d3
ruibosun <97702222+ruibosun@users.noreply.github.com>
2023-03-19 21:53:00 -0400

    add tests and simulate the data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,53 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Data cleaning for GSS 2021
+# Author: Ruibo Sun
+# Data: 16 March 2023
+# Contact: ruibo.sun@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Download all the packges under the Workspace section
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(123) # set seed for reproducibility
+
+# generate 100 observations
+n <- 100 
+
+# generate parents' education level, with higher levels of education having a greater probability of occurrence
+parent_edu <- sample(c("less than high school", "high school", "some college", "bachelor's degree", "graduate degree"), 
+                     n, 
+                     replace = TRUE)
+
+# generate a binary variable indicating whether parents have higher education or not
+parent_higher_edu <- ifelse(parent_edu %in% c("bachelor's degree", "graduate degree"), 1, 0)
+
+# generate children's education, with higher probability of occurrence if parents have higher education
+child_edu <- sample(c("less than high school", "high school", "some college", "bachelor's degree", "graduate degree"), 
+                    n, 
+                    replace = TRUE)
+
+# generate a binary variable indicating whether children have higher education or not
+child_higher_edu <- ifelse(child_edu %in% c("bachelor's degree", "graduate degree"), 1, 0)
+
+# generate a variable indicating region of living during teenage age, with equal probability of occurrence
+region <- sample(c("North", "South", "East", "West"), n, replace = TRUE)
+
+# generate a random prestige score for parents, with higher education level and living in certain regions associated with higher scores
+prestige_score <- ifelse(parent_edu %in% c("bachelor's degree", "graduate degree"), 
+                         rnorm(n, mean = 75, sd = 15), 
+                         rnorm(n, mean = 50, sd = 10))
+prestige_score <- ifelse(region == "East", prestige_score + rnorm(n, mean = 10, sd = 5), prestige_score)
+prestige_score <- ifelse(region == "West", prestige_score - rnorm(n, mean = 10, sd = 5), prestige_score)
+
+# create a data frame with the generated variables
+simulated_data <- data.frame(parent_edu, parent_higher_edu, child_edu, child_higher_edu, region, prestige_score)
+
+
 
 
 
--- scripts/04-test_data.R
+++ scripts/04-test_data.R
@@ -1,15 +1,47 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Data cleaning for GSS 2021
+# Author: Ruibo Sun
+# Data: 16 March 2023
+# Contact: ruibo.sun@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Download all the packges under the Workspace section
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+cleaned_data <- read_csv(
+  file = here("inputs/data/GSS2021 2.csv"))
 
 #### Test data ####
+cleaned_data$mapres10|> class() == "numeric"
+
+cleaned_data$papres10|> class() == "numeric"
+
+cleaned_data$paeduc|> class() == "numeric"
+
+cleaned_data$maeduc|> class() == "numeric"
+
+cleaned_data$educ|> class() == "numeric"
+
+cleaned_data$mawrkslf|> class() == "character"
+
+cleaned_data$pawrkslf|> class() == "character"
+
+cleaned_data$degree|> class() == "character"
+
+cleaned_data$born|> class() == "character"
+
+cleaned_data$sex|> class() == "character"
+
+cleaned_data$degree|> class() == "character"
+
+cleaned_data$incom16|> class() == "character"
+
+cleaned_data$reg16|> class() == "character"
+
+cleaned_data$maeduc|> min(na.rm = T) >= 0
+
+cleaned_data$paeduc|>min(na.rm = T) >= 0
+
+cleaned_data$educ|>min(na.rm = T) >= 0
